eb0e0825e187fe495d17873a2bd2461077470cac
cmillauriaux <cmillauriaux@icysoft.fr>
2013-10-17 16:15:01 +0200

    Initial commit Adding dependecies at Spring, Spring Data and MongoDB to pom.xml Writting main class "App.java" with connexion to MongoDB and logs

---
+++ SpringDataMongoDB-BaseExample/src/main/java/org/icysoft/blog/example/springdatamongodb/App.java
@@ -0,0 +1,36 @@
+package org.icysoft.blog.example.springdatamongodb;
+
+import com.mongodb.Mongo;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+/**
+ * Hello world!
+ *
+ */
+@Configuration
+@EnableMongoRepositories
+public class App {
+
+    @Bean
+    protected static Mongo mongo() throws UnknownHostException {
+        return new Mongo("localhost");
+    }
+
+    public static void main(String[] args) {
+        try {
+            Logger.getLogger(App.class.getName()).log(Level.INFO, "Trying to connect to MongoDB");
+            Mongo mongoDb = App.mongo();
+            Logger.getLogger(App.class.getName()).log(Level.INFO, "Connected to {0} version {1}", new Object[]{mongoDb.getConnectPoint(), mongoDb.getVersion()});
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, "Impossible to reach MongoDB", ex);
+        }
+    }
+}
